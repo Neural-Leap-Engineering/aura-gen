46ed0ec12a1e7a7cb9bc451dff915e26ddecdf27
Neural-Leap-Engineering <engineering@neuralleap.Ai>
2023-10-22 10:16:34 +0530

    Final commit

---
+++ README.md
@@ -0,0 +1,64 @@
+---
+title: V6 Auragen
+emoji: 🌍
+colorFrom: indigo
+colorTo: gray
+sdk: gradio
+sdk_version: 3.50.2
+app_file: app.py
+pinned: false
+---
+
+# Aura-Gen Article Generator
+
+Aura Gen Article Generator is a tool that allows you to generate articles on the fly. Whether you're a content creator, marketer, or simply looking for an article generation solution, Aura Gen has got you covered. With Aura Gen, you can quickly and easily generate articles based on your input, making content creation more efficient and hassle-free.
+
+## Getting Started
+
+To get started with Aura Gen Article Generator, follow these simple steps:
+
+### 1. Acquire API Keys
+
+- **Rapid API Key**: You need to obtain a Rapid API Key from the following URL: [Rapid API Key](https://rapidapi.com/ajithjojo1230/api/seo-keyword-research/pricing). This key is required for accessing the article generation service.
+
+- **OpenAI Key**: You should also have an OpenAI API Key to ensure seamless integration with Aura Gen.
+
+- **Clipdrop Key**: You need to obtain a Clipdrop API Key from the following URL: [Clipdrop API Key](https://clipdrop.co/apis). This key is required for accessing the image generation service.
+
+- **AWS S3 Bucket Access Keys**: If you plan to store generated articles or related data, you will need the appropriate AWS S3 Bucket Access Keys.
+
+### 2. Install Requirements
+
+Before running the Gradio app, make sure you have the necessary dependencies installed. You can install them by using the provided `requirements.txt` file.
+
+```bash
+pip install -r requirements.txt```
+```
+### 3. Run the Gradio App
+
+With the prerequisites in place, you can now run the Gradio app for Aura Gen Article Generator. Execute the following command:
+
+`python app.py` 
+
+This command will start the Gradio app, and you will be able to access the article generation service at the provided URL.
+
+## Usage
+
+Once the Gradio app is up and running, visit the URL it provides to start using the Aura Gen Article Generator. You can input your content requirements, and Aura Gen will swiftly generate articles tailored to your needs.
+
+## Support and Feedback
+
+If you encounter any issues, have questions, or want to provide feedback, please don't hesitate to contact us. Your input is valuable in helping us improve Aura Gen Article Generator.
+
+## License
+
+This project is licensed under the [MIT License](https://chat.openai.com/c/LICENSE).
+
+----------
+
+Thank you for choosing Aura Gen Article Generator. We hope you find this tool valuable for your article generation needs. Happy writing!
+
+Hugging Face Link For The App :- https://huggingface.co/spaces/aura-gen/V6-auragen
+
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,218 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+import subprocess
+import gradio as gr
+import requests
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+import os
+from PIL import Image
+from io import BytesIO
+import requests
+from PIL import Image
+from io import BytesIO
+import openai
+import json
+import requests
+import boto3
+from io import BytesIO
+from PIL import Image
+import json
+import gradio as gr
+import imgkit
+import base64
+import uuid
+import os
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+
+
+api_key = os.environ.get("api_key")
+
+openai_api_key = "Add_Open_Api_Key"
+
+model_name = "gpt-3.5-turbo"
+
+rapid_api_key = "Add_rapid_api_key"
+
+def generate_title_and_subtitles(prompt, model_name, keyword):
+    chat_model = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model_name=model_name)
+    
+    # Generate content with keyword
+    messages = [HumanMessage(content=prompt+keyword)]
+    content = chat_model.predict_messages(messages).content
+    
+    # Try to split the content into title and subtitle based on the keyword
+    try:
+        title, subtitle = content.split(keyword, 1)
+    except ValueError:
+        # Handle the case when the keyword is not found
+        title = content
+        subtitle = content
+    
+    # Remove leading and trailing spaces from title and subtitle
+    title = title.strip()
+    subtitle = subtitle.strip()
+
+    struture=[{'title': title, 'subtitles': [subtitle]}]
+    print(struture)
+    # Return title and subtitle as a tuple
+    return struture
+
+def generate_images(structure):
+    # Create a dictionary to store S3 file paths grouped by title
+    image_dict = {}
+
+    # Iterate through each article
+    for i, art in enumerate(structure):
+        title = art['title']
+
+        # Make a request to generate the image for the title
+        r_title = requests.post('https://clipdrop-api.co/text-to-image/v1',
+            files={
+                'prompt': (None, title, 'text/plain')
+            },
+            headers={'x-api-key': api_key}
+        )
+
+        if r_title.ok:
+            image_bytes_title = BytesIO(r_title.content)
+            image_title = Image.open(image_bytes_title)
+
+            # Reset the position of the BytesIO object to the beginning
+            image_bytes_title.seek(0)
+
+            # Specify the file path and format for the title
+            title_file_path = f'generated_title_image_{i}.png'
+            image_title.save(title_file_path)
+
+            s3 = boto3.client('s3')
+            bucket_name = 'auragenv1'
+            content_type = 'image/png'
+
+            # Save the image to S3
+            s3.upload_file(title_file_path, bucket_name, title_file_path, ExtraArgs={'ContentType': content_type})
+
+            # Add the S3 file path to the image_dict
+            if title in image_dict:
+                image_dict[title].append(f'https://auragenv1.s3.amazonaws.com/{title_file_path}')
+            else:
+                image_dict[title] = [f'https://auragenv1.s3.amazonaws.com/{title_file_path}']
+
+        else:
+            r_title.raise_for_status()
+
+        # Iterate through the subtitles for each article
+        for j, subtitle in enumerate(art['subtitles']):
+            # Make a request to generate the image for the subtitle
+            r_subtitle = requests.post('https://clipdrop-api.co/text-to-image/v1',
+                files={
+                    'prompt': (None, subtitle, 'text/plain')
+                },
+                headers={'x-api-key': api_key}
+            )
+
+            if r_subtitle.ok:
+                image_bytes_subtitle = BytesIO(r_subtitle.content)
+                image_subtitle = Image.open(image_bytes_subtitle)
+
+                # Reset the position of the BytesIO object to the beginning
+                image_bytes_subtitle.seek(0)
+
+                # Specify the file path and format for the subtitle
+                subtitle_file_path = f'generated_subtitle_image_{i}_{j}.png'
+                image_subtitle.save(subtitle_file_path)
+
+                # Save the image to S3
+                s3.upload_file(subtitle_file_path, bucket_name, subtitle_file_path, ExtraArgs={'ContentType': content_type})
+
+                # Add the S3 file path to the image_dict
+                if title in image_dict:
+                    image_dict[title].append(f'https://auragenv1.s3.amazonaws.com/{subtitle_file_path}')
+                else:
+                    image_dict[title] = [f'https://auragenv1.s3.amazonaws.com/{subtitle_file_path}']
+
+            else:
+                r_subtitle.raise_for_status()
+
+    dict_as_string = json.dumps(image_dict)
+    return dict_as_string
+
+def generate_html(dict_as_string):
+
+    
+
+    openai.api_key = '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'
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": dict_as_string+", using this multiple dictionary data create html article structure including title subtitle and also the paragraph with image component. use image size width 300px height 300px and center aligned. use all components center align and responsive"}
+    ],
+    temperature=0.8,
+    max_tokens=1500
+    )
+
+    # Extracting assistant's response
+    assistant_response = response['choices'][0]['message']['content']
+
+    #print(assistant_response)
+    print(assistant_response)
+    return assistant_response
+
+
+def final(key_word, num_terms):
+    key_word_list = rapid_api(key_word) if key_word else []
+    
+    combined_data = []
+    # Loop through each keyword to generate titles and subtitles
+    for keyword in key_word_list[:int(num_terms)]:
+        article_data = generate_title_and_subtitles(keyword, model_name, keyword)
+        combined_data.extend(article_data)
+
+    # Generate images for the combined data
+    combined_script = generate_images(combined_data)
+
+    # Generate the HTML structure based on the data and images
+    final_html = generate_html(combined_script)
+
+    with open("output.html", "w") as html_file:
+        html_file.write(final_html)
+
+    return gr.HTML(final_html)
+    
+
+def rapid_api(key_word):
+    url = "https://seo-keyword-research.p.rapidapi.com/keynew.php"
+    querystring = {"keyword": key_word, "country": "in"}
+    headers = {
+        "X-RapidAPI-Key": rapid_api_key,
+        "X-RapidAPI-Host": "seo-keyword-research.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        data = response.json()
+        text_values = [item['text'] for item in data]
+        return text_values
+    else:
+        return []
+    
+
+interface = gr.Interface(
+    fn=final, 
+    inputs=[
+        gr.Textbox(label="Key Word", placeholder="Enter keyword..."),
+        gr.Number(label="Number of Terms")
+    ],
+    outputs=gr.HTML(label="Generated articles will appear here...")
+)
+
+if __name__ == "__main__":
+    interface.launch()
+
+# Example usage
+#keyword = "toyota prius"
+#prompt = keyword
+#article_data = generate_title_and_subtitles(prompt, model_name, keyword)
+#print(article_data)
